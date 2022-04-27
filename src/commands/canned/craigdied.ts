8673a9906499932e8a0040829cca9e3130be9fcd
Snazzah <suggesttosnazzy@gmail.com>
2022-04-27 18:54:36 -0500

    feat: update craigdied canned message

--- src/commands/canned/craigdied.ts
+++ src/commands/canned/craigdied.ts
@@ -14,7 +14,8 @@ export default class Craigdied extends CannedMessage {
           We are working on a solution to make Craig a lot more stable, but we tend to battle the issues Discord throws at us since the bot is pretty large, so bear with us.
 
           For now, you can try the following:
-          - Re-record your voice channel when you get the "unexpected disconnect" message.
+          - **Use the public beta of Craig here: <#958603499539685377>**
+          - Re-record your voice channel when you get the "unexpected disconnect" message, and pay attention to your DMs during the recording.
           - [Invite Giarc](https://discordapp.com/oauth2/authorize?client_id=486698344429781028&scope=bot+applications.commands), it seems to be more stable.
           - Use Alistair if you have access.
         `
