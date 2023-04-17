55e8aa1f5e8495f3a8ed01fba706b489db72d167
Ralph <blackbirdralph@aol.com>
2023-04-17 14:30:41 -0400

    Update soundboard.ts

--- src/commands/canned/soundboard.ts
+++ src/commands/canned/soundboard.ts
@@ -7,8 +7,12 @@ export default class Site extends CannedMessage {
     embeds: [
       {
         title: 'Soundboard Support',
-        url: 'https://craig.chat',
-        description: "Craig's website: https://craig.chat"
+        description: stripIndents`
+        At the moment we've not looked into any support for Soundboard.
+        There are multiple factors to this, but a large piece is how new it is, and the lack of proper documentation of Discord's Voice API. 
+        
+        IF we decide to implement support, it will likely be a while.
+        `
       }
     ]
   };
@@ -16,8 +20,8 @@ export default class Site extends CannedMessage {
   constructor(client: DexareClient<any>) {
     super(client, {
       name: 'soundboard',
-      description: 'At the moment we've not looked into any support for Soundboard. There are multiple factors to this, but a large piece is how new it is, and the lack of proper documentation of Discord's Voice API. IF we decide to implement support, it will likely be a while.',
-      aliases: ['website']
+      description: 'Explain our support for the new Soundboard feature.'
+      aliases: ['sb']
     });
 
     this.filePath = __filename;
