9623949c53cb9e7efd5f40113164de9195cb4e6b
Snazzah <me@snazzah.com>
2025-09-18 13:25:18 -0400

    Update vcmod can

--- src/canned/vcmod.ts
+++ src/canned/vcmod.ts
@@ -12,6 +12,8 @@ export default {
         description: stripIndents`
           Craig is not intended for voice channel moderation purposes. It's intended use is for recording sessions like podcasts and videos.
           Be mindful of this when using Craig.
+
+          One of the maintainers of Craig (Snazzah) is developing a voice moderation solution at https://audrey.gg that may suit your needs.
         `,
       },
     ],
